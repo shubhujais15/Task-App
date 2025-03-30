8406c98863f63b7082b8581af6bddbd47f237c77
shubhujais15 <shubhujaisedu15@gmail.com>
2025-03-30 16:41:15 +0530

    Create Task project

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require("path");
+const fs = require("fs")
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname,"public")));
+
+
+app.get('/',(req,res)=>{
+    fs.readdir(`./files`, (err,files)=>{
+        // console.log(files);
+        res.render("index", {files: files});
+    })
+    
+})
+
+app.post('/create',(req,res)=>{
+   fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, (err)=>{
+    res.redirect('/')
+   })
+    
+})
+
+app.get('/files/:filename',(req,res)=>{
+    fs.readFile(`./files/${req.params.filename}`, "utf-8", (err,filedata)=>{
+        res.render('show', {filename: req.params.filename, filedata: filedata})
+    })
+    
+})
+
+
+app.listen(3000,()=>{
+    console.log("Port Started at 3000");
+})
